5fd931e85cf0f9a80235a5324f46e1e208b68999
Amir Majeed <amirmajeed4905@gmail.com>
2025-11-14 20:30:54 +0500

    update style product page..

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.get("/", (req, res) => {
 app.use((req, res, next) => {
     res.status(404).render("404.ejs");
 });
-app.listen(port, () => {
+app.listen(port || 4000, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
